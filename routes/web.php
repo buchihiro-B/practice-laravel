d0d0b8450a9680401f88981c08dba7d3b100fc5d
buchihiro <buchihiro5147gaming@gmail.com>
2022-09-19 05:19:38 +0900

    create graph

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WeightGraphController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/graph', [WeightGraphController::class, "show"])->name("show_graph");
